22338f055e1b10b792a12b715028350da4e2f734
Alexandr <87748202+shadowHD-bit@users.noreply.github.com>
2023-02-15 21:30:28 +0700

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = 8080;
+
+// приложение
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.set("views", path.join(__dirname, "public/views"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    text: "Hello World and Docker!",
+    lorem:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quae, neque ab ratione cupiditate, officiis dignissimos sint accusamus assumenda atque consequuntur praesentium eius nulla nisi dolore numquam temporibus, tempore quibusdam!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Application running...`);
+});
